2e75536097cc839aeff1ff0977429f3a149b49df
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-08-05 16:33:08 -0300

    Criação das funções para inserir amigos e atualizar o conteúdo html no página

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 
-var listaAmigos = [];
+let listaAmigos = [];
 
 
 
+function adicionarAmigo(){
+    let campo = document.querySelector('input');
+    let nomeAmigo = campo.value.trim();
+    if(nomeAmigo == null){
+        alert('Porfavor, insira um nome!');
+    }
+
+    listaAmigos.push(nomeAmigo);
+    campo.innerHTML = '';
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos(){
+    let campoListaAmigos = document.getElementById('listaAmigos');
+    campoListaAmigos.innerHTML = '';
+    for(i = 0; i < listaAmigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[i];
+        campoListaAmigos.appendChild(li);
+    }
+
+
+function sortearAmigo(){
+    if(listaAmigos.length == 0){
+        alert("A lista de amigos está vazio,adicione amigos!");
+    }
+}
+
+}
